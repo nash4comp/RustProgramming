6ca1cbe715a566982a3883d8df2182ddbde8d8da
Nash Baek <nash4comp@gmail.com>
2023-09-26 14:59:31 -0700

    Finish rust lab2-1

--- lab/lab2/sort/src/main.rs
+++ lab/lab2/sort/src/main.rs
@@ -1,7 +1,16 @@
+// Rust Lab 2-1
+// Nash Baek, A01243888
+
+// Criteria
+// 1. Descending order of scores.
+// 2. If same above, ascending order of last names
+// 3. If same above, ascending order of first names
+
+// Import necessary modules
 use std::env;
+use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::fmt;
 
 // Define a struct to hold a record
 struct Record {
@@ -18,14 +27,20 @@ impl fmt::Display for Record {
 }
 
 fn main() -> std::io::Result<()> {
+    // Collect command line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("Usage: ./sort <filename>");
         return Ok(());
     }
 
+    // Get the filename from command line arguments
     let filename = &args[1];
+
+    // Open the file
     let file = File::open(filename)?;
+
+    // Create a buffered reader for the file
     let reader = BufReader::new(file);
 
     let mut records: Vec<Record> = Vec::new();
@@ -45,19 +60,28 @@ fn main() -> std::io::Result<()> {
             _ => continue,
         };
 
+        // Create a record from parsed data
         let record = Record {
             first_name: words[0].to_string(),
             last_name: words[1].to_string(),
             score,
         };
 
+        // Push the record into the vector
         records.push(record);
     }
 
-    // Sort the records
+    // Sort the records in descending order based on the score,
+    // and in case of a tie, sort by last name, and then by first name.
     records.sort_by(|a, b| {
-        b.score.cmp(&a.score)
+        // Compare the scores in descending order (higher scores first)
+        // using the `cmp` method.
+        b.score
+            .cmp(&a.score)
+            // If scores are equal, compare by last names in ascending order.
+            // This sorts names alphabetically from A to Z.
             .then(a.last_name.cmp(&b.last_name))
+            // If last names are also equal, compare by first names in ascending order.
             .then(a.first_name.cmp(&b.first_name))
     });
 
